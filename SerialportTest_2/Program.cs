bda01c067d80664218f455d756cc6d088e044a69
Koson Trachu <koson.trachu@gmail.com>
2016-01-28 22:07:01 +0700

    1st

---
+++ SerialportTest_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.IO.Ports;
+
+
+namespace SerialportTest_2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.Print("Hello Netduino\r\n");
+            // write your code here
+        }
+    }
+}
